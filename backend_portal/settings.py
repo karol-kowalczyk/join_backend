ceed1798e4e34482aad63a3da80680f7f80b2c7b
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-02-28 22:46:35 +0100

    static root importetd

--- backend_portal/settings.py
+++ backend_portal/settings.py
@@ -29,15 +29,7 @@ ALLOWED_HOSTS = [
     '34.90.144.16'
 ]
 
-import os
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Falls du ein 'static'-Verzeichnis im Projekt hast
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Für Collectstatic
 
 
 # Application definition
@@ -134,7 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Falls du ein 'static'-Verzeichnis im Projekt hast
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Für Collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
