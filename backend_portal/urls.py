d3c44d840c465da3ee93b865db28e4f793db9af7
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-02-28 22:52:04 +0100

    static root importetd

--- backend_portal/urls.py
+++ backend_portal/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication_app.api.urls')),
     path('database/', include('service_management_db.api.urls')),
     path('auth', include('rest_framework.urls'))
-]
+] + staticfiles_urlpatterns()
